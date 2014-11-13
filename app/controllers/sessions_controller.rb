c1ed5fa5254eb778505aec8cfd6edc1450cd2e3e
deb_akiya <akiyastyle@gmail.com>
2014-11-13 07:03:06 +0000

    rails tutorial 9.3

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,11 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-		user = User.find_by( email: params[ :email ].downcase )
-		if user && user.authenticate( params[ :password ] )
+		user = User.find_by( email: params[ :session ][ :email ].downcase )
+		if user && user.authenticate( params[ :session ][ :password ] )
 		    # ユーザーをサインインさせ、ユーザーページ (show) にリダイレクトする。
 		    sign_in user
-		    redirect_to user
+		    redirect_back_or user
 		else
 		    # エラーメッセージを表示し、サインインフォームを再描画する。
 		    flash.now[ :error ] = 'Invalid email/password combination'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [ :edit, :update ]
+  before_action :signed_in_user,	only: [ :index, :edit, :update ]
+  before_action :correct_user,		only: [ :edit, :update ]
+
+  def index
+  	@users = User.all
+  end
 
   def show
   	@user = User.find( params[:id] )
@@ -21,11 +26,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-  	@user = User.find( params[ :id ] )
   end
 
   def update
-  	@user = User.find( params[ :id ] )
   	if @user.update_attributes( user_params )
 		# 更新に成功した場合を扱う
 		flash[ :success ] = "Profile updated"
@@ -42,12 +45,17 @@ class UsersController < ApplicationController
   	end
 
   	# Before actions
+
   	def signed_in_user
-  		redirect_to signin_url, notice: "Please sign in." unless signed_in?
-  		# 冗長に書くとこう
-  		# unless signed_in?
-  		# 	flash[:notice] = "Please sign in."
-  		# 	redirect_to signin_url
-  		# end
+  		unless signed_in?
+  			store_location
+  			redirect_to signin_url, notice: "Please sign in."
+  		end
   	end
+
+  	def correct_user
+  		@user = User.find( params[ :id ] )
+  		redirect_to( root_path ) unless current_user?( @user )
+  	end
+
 end
