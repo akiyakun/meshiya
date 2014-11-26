b33431b1d98dfc3cab357cf5a6d7c7149582894f
deb_akiya <akiyastyle@gmail.com>
2014-11-26 11:23:17 +0000

    support I18n :ja :en

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,19 @@
 class ApplicationController < ActionController::Base
+	include SessionsHelper
+
 	# Prevent CSRF attacks by raising an exception.
 	# For APIs, you may want to use :null_session instead.
 	protect_from_forgery with: :exception
-	include SessionsHelper
+
+	before_action :set_locale
+
+	# デフォルトのURLにロケール情報を入れる
+	def default_url_options( options={} )
+		{ :locale => I18n.locale }
+	end
+
+	def set_locale
+		I18n.locale = params[:locale] || I18n.default_locale
+		# I18n.locale = params[:locale]
+	end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
 	# I18n macros
-	def t( key, options={} )
-	  I18n.t( key, options )
-	end
+	# def t( key, options={} )
+	#   I18n.t( key, options )
+	# end
 
 	# ページごとの完全なタイトルを返します。
 	def full_title( page_title )
-		base_title = t( 'app.title' )
+		base_title = I18n.t( 'app.title' )
 		if page_title.empty?
 			base_title
 		else
